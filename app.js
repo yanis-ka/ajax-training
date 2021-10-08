bbce5d6ae01faf10395abe326ed4b5f81021a6b8
yanis khellaf <yanis.khellaf@outlook.fr>
2021-10-08 14:46:27 +0200

    Simple users example done (no style)

---
+++ app.js
@@ -0,0 +1,32 @@
+const httpRequest = new XMLHttpRequest()
+const links = document.querySelectorAll('.users')
+let result = document.getElementById('result')
+
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault()
+        result.innerHTML = 'chargement...'
+        httpRequest.onreadystatechange = (e) => {
+            if (httpRequest.readyState === 4) {
+                result.innerHTML = ''
+                if (httpRequest.status === 200) {
+                    let results = JSON.parse(httpRequest.responseText)
+                    const ul = document.createElement('ul')
+                    result.appendChild(ul)
+                    results.forEach(result => {
+                        let li = document.createElement('li')
+                        li.innerHTML = result.name
+                        ul.appendChild(li)
+                    });    
+                } else {
+                    alert('Impossible de contacter le server')
+                }
+            }
+        }
+                
+        httpRequest.open('GET', 'https://jsonplaceholder.typicode.com/users', true)
+        httpRequest.send()
+    })
+});
+
+
